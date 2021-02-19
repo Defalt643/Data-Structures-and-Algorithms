a70e638100de7d97ca923775c67afb3e1007bdb5
Defalt <71940630+Defalt643@users.noreply.github.com>
2021-02-19 16:07:15 +0700

    Update

---
+++ src/Week9/HashMap_2.java
@@ -0,0 +1,10 @@
+package Week9;
+
+public class HashMap_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Week9/SeparateChainingHashST.java
+++ src/Week9/SeparateChainingHashST.java
@@ -1,35 +1,51 @@
 package Week9;
 
-public class SeparateChainingHashST <Key,Value>{
-	private int M = 8;
-	private Value[] vals = (Value[]) new Object[M];
-	private Key[] keys = (Key[]) new Object[M];
+public class SeparateChainingHashST <Key,Value> {
+	private int M = 5;
 	private Node[] st = new Node[M];
+	
 	private static class Node{
 		private Object key;
-		private Object Value;
+		private Object val;
 		private Node next;
-	}private int hash(Key key) {
-		return (key.hashCode() & 0x7fffffff) % M;
-	}public void put(Key key,Value val) {
-		int i =hash(key);
-		for(Node x = st[i];x!=null;x=x.next) {
+		
+		public Node(Object key2,Object val2,Node node) {
+			key = key2;
+			val = val2;
+			next = node;
+		}
+	}
+
+	private int hash(Key key){
+		return(key.hashCode() & 0x7FFFFFFF)% M;
+	}
+	
+	public Value get(Key key) {
+		int i = hash(key);
+		for(Node x = st[i]; x != null; x = x.next)
+			if(key.equals(x.key))
+				return(Value) x.val;
+		return null;
+	}
+	
+	public void put(Key key,Value val) {
+		int i = hash(key);
+		for(Node x = st[i]; x != null; x = x.next) {
 			if(key.equals(x.key)) {
-				x.Value=val;
+				x.val = val;
 				return;
 			}
 		}
-		st[i] = new Node(key, val,st[i]);
-	}public Value get(Key key) {
-		int i = hash(key);
-		for(Node x = st[i]; x!=null;x=x.next)
-			if(key.equals(x.key))
-				return (Value) x.Value;
-		return null;
-	}public void printNode() {
-		int i=4;
-		for(Node x = st[i];x !=null;x=x.next) {
-			System.out.println(i+" -> "+x.key+" "+x.Value);
-		}
+		st[i] = new Node(key,val,st[i]);
 	}
+	
+	public void printNode() {
+        int i = 0;
+        for(i = 0; i < st.length;i++) {
+            for(Node x= st[i];x != null;x = x.next){
+                System.out.println(i+" -> "+x.key+" "+x.val);
+            }
+        }
+	}
+
 }
